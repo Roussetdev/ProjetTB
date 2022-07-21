08f6f88cf41848b65bda28c7903c13892b0c5341
Rouset Dylan <dydy.rousset@gmail.com>
2022-07-21 14:45:08 +0200

    ajout du "else"

--- FRONT/STATIQUE/CSS/Jarditou/assets/js/javascript.js
+++ FRONT/STATIQUE/CSS/Jarditou/assets/js/javascript.js
@@ -5,7 +5,10 @@ console.log(reponse2);
 if (window.confirm("Etes-vous un homme ?") == true) 
 { 
     console.log("Homme");
-    var genre = "Monsieur "
+    var genre = "Monsieur ";
+}else{
+    console.log("femme");
+    var genre = "Madame ";
 }
 window.alert("Bonjour " + genre + reponse1 + " " + reponse2 + " \rBienvenue sur notre site web !" );
 let a = "100";
