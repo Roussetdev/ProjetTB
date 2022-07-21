7947fa142b01dc9495efe48d5fb4e086ce7013c5
Rouset Dylan <dydy.rousset@gmail.com>
2022-07-21 11:16:56 +0200

    exercice phase5 javascript fini

--- FRONT/STATIQUE/CSS/Jarditou/assets/js/javascript.js
+++ FRONT/STATIQUE/CSS/Jarditou/assets/js/javascript.js
@@ -1,6 +1,10 @@
 var reponse1 = window.prompt("Entrez votre nom");
+console.log(reponse1);
 var reponse2 = window.prompt("Entrez votre prenom");
+console.log(reponse2);
 if (window.confirm("Etes-vous un homme ?") == true) 
 { 
-    
+    console.log("Homme");
+    var genre = "Monsieur "
 }
+window.alert("Bonjour " + genre + reponse1 + " " + reponse2 + " \rBienvenue sur notre site web !" );
