164b1cd5d465d33fbb69b40ef9ea68747c3e48b5
Rouset Dylan <dydy.rousset@gmail.com>
2022-09-01 16:57:21 +0200

    exercice 1 eval

---
+++ FRONT/STATIQUE/CSS/Eval Java/exo1.js
@@ -0,0 +1,9 @@
+    // Evaluation
+
+    // exercice 1
+    var bouton1= document.getElementById("Id_btn1");
+    bouton1.addEventListener("click",clickbtn1);
+    
+    function clickbtn1(){
+        //
+    }
