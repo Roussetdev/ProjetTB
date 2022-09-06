e421fb289521993a373cdd8b9b7e09ebe7339d33
Rouset Dylan <dydy.rousset@gmail.com>
2022-09-06 15:57:02 +0200

    ex2 eval js debut

---
+++ FRONT/STATIQUE/CSS/Eval Java/exo2.js
@@ -0,0 +1,9 @@
+    // Evaluation
+
+    // exercice 2
+    var bouton2= document.getElementById("Id_btn2");
+    bouton2.addEventListener("click",clickbtn2);
+    
+    function clickbtn2(){
+        // variables
+    }
