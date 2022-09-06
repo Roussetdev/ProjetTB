67f67be614f2fee58e2e6878c61b030db62261e9
Rouset Dylan <dydy.rousset@gmail.com>
2022-09-06 15:54:27 +0200

    exo 1 eval js

--- FRONT/STATIQUE/CSS/Eval Java/exo1.js
+++ FRONT/STATIQUE/CSS/Eval Java/exo1.js
@@ -5,5 +5,30 @@
     bouton1.addEventListener("click",clickbtn1);
     
     function clickbtn1(){
-        //
+        // variables
+        let jeune = 0;
+        let mature = 0;
+        let vieux = 0;
+        let age = 0;
+        
+        // tant que age < 100
+        while(age < 100){
+            age = prompt("Entrez un âge "); 
+            console.log(age);
+                //ajout d'une personne jeune
+            if (age <= 20){
+                jeune = jeune +1;
+                console.log(jeune);
+            }   //ajout d'une personne mature
+            else if(age > 20 && age <= 40){
+                mature = mature + 1;
+                console.log(mature);
+            }   //ajout d'une personne assez vieux
+            else if(age > 40 ){
+                vieux = vieux + 1;
+                console.log(vieux);
+            }
+        }
+        
+        alert("jeune(s) : "+ jeune +" mature(s) : "+ mature +" vieux : "+ vieux);
     }
--- FRONT/STATIQUE/CSS/test javascript/finbasejs/scriptphase10.js
+++ FRONT/STATIQUE/CSS/test javascript/finbasejs/scriptphase10.js
@@ -5,5 +5,8 @@ var bouton3= document.getElementById("Id_btn3");
 bouton3.addEventListener("click",clickbtn3);
 
 function clickbtn3(){
-    //
+    let tableau = prompt("Taille du tableau ");
+    let valeurs = prompt("Entrez les différentes valeurs "); 
+
+     
 }
