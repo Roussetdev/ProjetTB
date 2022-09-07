992707b3385e7bf147b423b5c7d18c3397f7eac1
Rouset Dylan <dydy.rousset@gmail.com>
2022-09-07 10:57:29 +0200

    exo 3 eval js fini

--- FRONT/STATIQUE/CSS/Eval Java/exo3.js
+++ FRONT/STATIQUE/CSS/Eval Java/exo3.js
@@ -5,5 +5,15 @@
     bouton3.addEventListener("click",clickbtn3);
     
     function clickbtn3(){
-        // variables
+        // création d'un tableau avec différents prénoms
+        var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+        var prenom = prompt("Saisissez un prénom parmi Audrey, Aurélien, Flavien, Jérémy, Laurent, Melik, Nouara, Salem, Samuel, Stéphane");
+        // recherche du prénom saisie et supréssion de celui-ci
+        if(tab.includes(prenom)){
+            tab.splice(tab.indexOf(prenom),1);
+            tab.push(" ");
+            console.log(tab)
+            alert(tab)
+        }   // message d'erreur si annuler ou prénom incorrect
+        else{alert("erreur")}
     }
