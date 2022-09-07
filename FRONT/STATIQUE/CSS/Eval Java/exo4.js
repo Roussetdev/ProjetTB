d96fe33bb0ce1c6500cd1234e5833190c97fc237
Rouset Dylan <dydy.rousset@gmail.com>
2022-09-07 11:02:12 +0200

    exo 4 eval js debut

---
+++ FRONT/STATIQUE/CSS/Eval Java/exo4.js
@@ -0,0 +1,9 @@
+    // Evaluation
+
+    // exercice 4
+    var bouton4= document.getElementById("Id_btn4");
+    bouton4.addEventListener("click",clickbtn4);
+    
+    function clickbtn4(){
+        // variables
+    }
